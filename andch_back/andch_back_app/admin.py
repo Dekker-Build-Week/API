ba3912bb3b7ff06766a63d766e4b254fde63799c
amardeep <amardeep.dosanjh@and.digital>
2019-09-16 10:49:12 +0100

    created endpoint to return client information, needs tests

--- andch_back/andch_back_app/admin.py
+++ andch_back/andch_back_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from andch_back_app.models import Client
 
 # Register your models here.
+
+admin.site.register(Client)
--- andch_back/andch_back_app/views.py
+++ andch_back/andch_back_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from andch_back_app.models import Client
 
 # Create your views here.
+
+def all_clients(request):
+    clients = Client.objects.all()
+    clientList = []
+    for client in clients:
+        clientList.append({'clientName': client.clientName, 'imagePath': client.imagePath},)
+    data = {'clients:' : clientList}
+
+    return JsonResponse(data)
