156868813e7ac27459aa8371cc5912b59c1567b5
andrewpsuedonym <j.m.walker@live.co.uk>
2019-09-16 11:46:13 +0100

    create populate script for django models

---
+++ andch_back/populate.py
@@ -0,0 +1,30 @@
+## populate.py
+##
+## script to populate django's models with dummy data
+
+## run script with:      python populate.py to run
+## If data is corrupted, then first use:     python manage.py flush
+
+import django
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'andch_back.settings')
+django.setup()
+
+from andch_back_app.models import Client
+
+def import_clients():
+    Client.objects.get_or_create(clientName='Shawbrook',
+                         imagePath='andch_back/static/img')
+    Client.objects.get_or_create(clientName='British Airways',
+                         imagePath='andch_back/static/img')
+
+def main():
+    #Set up environment
+
+    print('Importing clients... ', end='')
+    import_clients()
+    print('done!')
+
+if __name__ == '__main__':
+    main()
