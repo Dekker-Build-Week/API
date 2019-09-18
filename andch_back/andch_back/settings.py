76a76e4a4dfcfc89564bf874fd173409695a0a48
andrewpsuedonym <j.m.walker@live.co.uk>
2019-09-18 10:02:48 +0100

    attempt to get static files working on heroku

--- andch_back/andch_back/settings.py
+++ andch_back/andch_back/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'andch_back_app/static/'
 
 CORS_ORIGIN_WHITELIST = [
          'http://localhost:3000'
