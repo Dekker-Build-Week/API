ca0709d9943684b5123578eb8a3b16e920970b86
amardeep <amardeep.dosanjh@and.digital>
2019-09-18 14:03:23 +0100

    Modified settings.py to test CircleCI fix

--- andch_back/andch_back/settings.py
+++ andch_back/andch_back/settings.py
@@ -85,6 +85,13 @@ WSGI_APPLICATION = 'andch_back.wsgi.application'
 
 DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+if len(DATABASES['default']) == 0:
+    DATABASES = {
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
--- andch_back/andch_back_app/models.py
+++ andch_back/andch_back_app/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 class Client(models.Model):
     clientName = models.CharField(max_length=100)
-    clientImagePath = models.ImageField(upload_to = 'clientLogos/')
+    clientImagePath = models.ImageField(upload_to = 'clientLogos/', help_text='Please ensure that any client logos include the company"s name in the logo')
+
+    class Meta:
+        verbose_name = 'Client'
 
     def __str__(self):
         return self.clientName
@@ -11,6 +14,9 @@ class Client(models.Model):
 class Andi(models.Model):
     andiName = models.CharField(max_length=150)
     andiPhotoPath = models.ImageField(upload_to = 'andis/')
+
+    class Meta:
+        verbose_name = 'ANDi'
 
     def __str__(self):
         return self.andiName
@@ -20,6 +26,9 @@ class Project(models.Model):
     projectTitle = models.CharField(max_length=100)
     projectDescription = models.TextField()
 
+    class Meta:
+        verbose_name = 'Project'
+
     def __str__(self):
         return self.projectTitle
 
@@ -27,16 +36,22 @@ class ProjectAndis(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     projectAndi = models.ForeignKey(Andi, on_delete=models.CASCADE)
 
+    class Meta:
+        verbose_name = 'Project ANDi'
+        verbose_name_plural = 'Project ANDis'
+
     def __str__(self):
         return self.project.projectTitle + ": " + self.projectAndi.andiName
 
 class ProjectImages(models.Model):
-    position = models.IntegerField()
+    position = models.IntegerField(help_text='Please enter a value of 0 for the cover image that will appear on the home page')
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     projectImagePath = models.ImageField(upload_to = 'site_images/')
 
     class Meta:
         ordering = ['position']
+        verbose_name = 'Project Image'
+        verbose_name_plural = 'Project Images'
 
     def __str__(self):
         return self.project.projectTitle + ": " + self.projectImagePath.url
@@ -45,10 +60,12 @@ class ProjectTechnologyStacks(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     technologyName = models.CharField(max_length=100)
     technologyImagePath = models.ImageField(upload_to = 'tech_images/')
-    important = models.BooleanField()
+    important = models.BooleanField(help_text='By ticking this value, the logo of the technology will appear on the home page')
 
     class Meta:
         ordering = ['-important']
+        verbose_name = 'Project Technology Stack'
+        verbose_name_plural = 'Project Technology Stacks'
     
     def __str__(self):
         return self.project.projectTitle + ": " + self.technologyName
